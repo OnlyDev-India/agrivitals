bbbe2718c09f3962ac11601ca9f3d2ba617f3ff6
Abhishek Dagar <dagarabhishek584@gmail.com>
2025-08-04 16:27:36 +0530

    changes the default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <Topbar />
           {children}
         </ThemeProvider>
